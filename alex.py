611a537944ac46e9d3649c5adb4b2e48ad000821
olgagolgan <67375574+olgagolgan@users.noreply.github.com>
2022-07-07 10:33:53 +0200

    queries

--- alex.py
+++ alex.py
@@ -6,37 +6,32 @@ alexithymia.parse("alexont.ttl", format="ttl")
 
 cq1 = '''
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
-select ?vacation where {?vacation a onto:Vacation}
+select ?statisticalTest where {?statisticalTest a alex:StatisticalTest}
 '''
 
 cq2 = '''
 PREFIX alex: <http://www.alexontology.com#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-select ?train ?plane where {?train a onto:Train.
-                            ?plane a onto:Plane }
+select ?clinicalDisorders where {?clinicalDisorders a alex:ClinicalDisorder}
 '''
 
 cq3 = '''
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?Trip where {?Trip a onto:Trip.
-                    ?Trip a onto:Group}
-'''
+ select ?questionnaires where {?questionnaires a alex:Questionnaire}
+ '''
 
 cq4 = '''
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?trainTrip ?planeTrip  where {?trainTrip a onto:trainTrip.
-                        ?trainTrip onto:hasBookedHotel ?hotel.
-                        ?planeTrip a onto:planeTrip.
-                        ?planeTrip onto:hasBookedHotel ?albergo
+select ?interoceptionBranches WHERE {?interoceptionBranches a alex:Interoception.
+                        FILTER (?interoceptionBranches != alex:InteroceptiveConfusion)
+                        FILTER (?interoceptionBranches != alex:Interoception)
                         }
 
 '''
@@ -46,14 +41,8 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?vacation ?vacation2 ?vacation3 ?vacation4 where {?vacation a onto:Vacation.
-                        ?vacation onto:hasTrip onto:DV_BolognaMadrid.
-                        ?vacation2 a onto:Vacation.
-                        ?vacation2 onto:hasTrip onto:DV_MadridAmsterdam.
-                        ?vacation3 a onto:Vacation.
-                        ?vacation3 onto:hasTrip onto:SV_AmstedamMadrid.
-                        ?vacation4 a onto:Vacation.
-                        ?vacation4 onto:hasTrip onto:SV_RomeAmsterdam   
+select ?nonAffectiveState where {?nonAffectiveState a alex:PhysicalState.
+                        ?nonAffectiveState alex:isConnectedTo alex:StateEmotionSImilarityQuestionnaire
                         }
 
 '''
@@ -64,8 +53,8 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?trainTrip ?duration where{ ?trainTrip a onto:trainTrip.
-                        ?trainTrip onto:HourDuration ?duration.
+select ?emotion where{ ?emotion a alex:Emotion.
+                        ?emotion alex:isConnectedTo alex:StateEmotionSImilarityQuestionnaire
 }
 '''
 
@@ -75,8 +64,8 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?stars where{?Hotel a onto:Hotel.
-                    ?Hotel onto:StarIndicator ?stars
+select ?brainRegion where{?brainRegion a alex:BrainRegion.
+                    ?brainRegion alex:isInvolvedIn alex:Interoception
 }
 '''
 
@@ -86,8 +75,7 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?Hotel where{?Hotel a onto:Hotel.
-                    ?Hotel onto:hasBreakfastIncluded true}
+select ?social where{alex:Participant alex:isRecruitedVia ?social}
 '''
 
 cq9 = '''
@@ -96,11 +84,8 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?Hotel ?trip where{
-                    ?Hotel a onto:Hotel.
-                    ?trip onto:hasBookedHotel ?Hotel;
-                     onto:FisherVacation_FV
-                    }
+select DISTINCT ?number where{?questionnaire a alex:Questionnaire.
+                            ?questionnaire alex:hasParticipantNumber ?number}
 '''
 
 cq10 = '''
@@ -109,10 +94,10 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX alex: <http://www.alexontology.com#>
 
-select ?trainTrip ?duration where {
-                    ?trainTrip a onto:trainTrip.
-                    ?trainTrip onto:hasBookedHotel onto:Multimedhostel.
-                    ?trainTrip onto:HourDuration ?duration.
+select ?scale where {
+                    ?scale a alex:Scale.
+                    FILTER (?scale != alex:StateEmotionSImilarityQuestionnaire)
+                    FILTER (?scale != alex:InteroceptiveConfusionScore)
                     }
 '''
 
@@ -120,6 +105,6 @@ cqs = [cq1, cq2, cq3, cq4, cq5, cq6, cq7, cq8, cq9, cq10]
 
 for idx, cq in enumerate(cqs):
     print("Results for cq " + str(idx + 1) + ":")
-    results = vacation_trips.query(cq)
+    results = alexithymia.query(cq)
     for result in results:
         print(result)
